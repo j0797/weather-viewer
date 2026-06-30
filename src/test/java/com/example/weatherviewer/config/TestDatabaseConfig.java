ddf9a7538a4ee4f676a0a70f58f434296cff7069
j0797 <anonymous@example.com>
2026-06-30 15:12:50 +0300

    chore: configure test infrastructure with H2

---
+++ src/test/java/com/example/weatherviewer/config/TestDatabaseConfig.java
@@ -0,0 +1,63 @@
+package com.example.weatherviewer.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@Profile("test")
+@PropertySource("classpath:application-test.properties")
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "com.example.weatherviewer.service",
+        "com.example.weatherviewer.repository"
+})
+public class TestDatabaseConfig {
+
+    private final Environment environment;
+
+    public TestDatabaseConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(environment.getProperty("db.url"));
+        config.setUsername(environment.getProperty("db.username"));
+        config.setPassword(environment.getProperty("db.password"));
+        config.setDriverClassName(environment.getProperty("db.driver"));
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.example.weatherviewer.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new HibernateTransactionManager(Objects.requireNonNull(sessionFactory().getObject()));
+    }
+
+    private Properties hibernateProperties() {
+        Properties props = new Properties();
+        props.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        props.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql", "true"));
+        props.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto", "create-drop"));
+        return props;
+    }
+}
